8cb578955ab66465f035c50abc4886223c7773ad
Jose Maria Aran Tejada <josee.arant@gmail.com>
2026-06-18 16:17:57 +0200

    Finished exercise 4b3: fixed imports and completed deepcopy validation

--- 4b/ej4b3.py
+++ 4b/ej4b3.py
@@ -82,8 +82,16 @@ def create_list(length_list):
     the second one created in Heap by reusing the first list.
     """
 
-    # Write here your code
-    pass
+    if length_list <= 0:
+        raise ValueError("The number must be positive")
+    ram_list = []
+    for _ in range(length_list):
+        ram_list.append(random.randint(0, 100))
+
+    heap_list = copy.deepcopy(ram_list)
+
+    return ram_list, heap_list
+    
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
